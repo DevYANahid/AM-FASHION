0af0e0a6429a76e197749ab8ee686c4635d08570
DevYANahid <codealgorithmschool@gmail.com>
2024-04-24 16:49:21 +0600

    login,register,reset password page added

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -467,9 +467,30 @@ PAGE JS
 		carousel_slider();
 		slick_slider();
 	});
+
+
+
+	/*===================================*
+	11. password show FORM JS
+	*===================================*/
+	const passwordField = document.getElementById("password");
+	const togglePassword = document.querySelector(".password-toggle-icon i");
+
+	togglePassword.addEventListener("click", function () {
+	if (passwordField.type === "password") {
+		passwordField.type = "text";
+		togglePassword.classList.remove("fa-eye");
+		togglePassword.classList.add("fa-eye-slash");
+	} else {
+		passwordField.type = "password";
+		togglePassword.classList.remove("fa-eye-slash");
+		togglePassword.classList.add("fa-eye");
+	}
+});
 	/*===================================*
 	11. CONTACT FORM JS
 	*===================================*/
+	
 	$("#submitButton").on("click", function(event) {
 	    event.preventDefault();
 	    var mydata = $("form").serialize();
